3b98185d2f12f0a302b6aff53aa035946e2718ba
venkateshvenky184 <venkateshthommandru5@gmail.com>
2020-08-15 14:07:46 +0530

    Sample problems

---
+++ Day_04_02.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 15 11:35:16 2020
+
+@author: venky
+"""
+import sqlite3
+
+class Database(object):
+    def __init__(self):
+        self.connection = sqlite3.connect("Database.db")
+        self.cursor = self.connection.cursor()
+        self.table_name = "Table_Example"
+        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name} \
+                            (column_1 INT, column_2 TEXT, column_3 REAL)")
+        
+    def insert_data(self):
+        column_1 = int(input("Enter Int: "))
+        column_2 = input("Enter String: ")
+        column_3 = float(input("Enter Float: "))
+        self.cursor.execute(f"INSERT INTO {self.table_name} \
+            (column_1, column_2, column_3) VALUES(?,?,?)",(column_1, column_2, column_3))
+        
+        self.connection.commit()
+        print("Data Saved")
+    
+    def read_data(self):
+        self.cursor.execute(f"SELECT * FROM {self.table_name}")
+        for row in self.cursor.fetchall():
+            print(row)
+            print("task completed")
+
+if __name__ == "__main__":
+    database = Database()
+    database.insert_data()
+    database.read_data()
+    database.cursor.close()
+    database.connection.close()
